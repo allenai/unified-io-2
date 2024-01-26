89ec67b47240c432de9278012aca5e1ed1f03d9b
Zichen "Charles" Zhang <52727818+zcczhang@users.noreply.github.com>
2024-01-26 14:21:17 -0800

    support ckpt path from gs

--- t5x/examples/unified_io/utils.py
+++ t5x/examples/unified_io/utils.py
@@ -1,3 +1,4 @@
+import os
 import time
 from typing import Optional, List, Dict, Mapping, Any, Sequence, Tuple
 
@@ -89,8 +90,9 @@ def get_parameters(model, model_checkpoint: str = None,
       param_axes = train_state_initializer.train_state_axes.params
       params = freeze(train_state.params)
   else:
-    model_checkpoint = os.path.abspath(os.path.expanduser(model_checkpoint))
-    assert os.path.exists(model_checkpoint), f"{model_checkpoint=} does not exist!"
+    if not model_checkpoint.startswith("gs://"):
+      model_checkpoint = os.path.abspath(os.path.expanduser(model_checkpoint))
+      assert os.path.exists(model_checkpoint), f"{model_checkpoint=} does not exist!"
     logging.info(f"Loading model weights from {model_checkpoint}...")
     input_shapes, input_types = get_input_spec(1)
     if partitioner is not None:
